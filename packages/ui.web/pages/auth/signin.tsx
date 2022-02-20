611afcca72b846f7ed97b276f68094c384e1dfdb
Daniel Mauricio Flores <dnielfs@gmail.com>
2022-02-20 23:13:41 +0100

    feature: add signup/signin ui.web pages

---
+++ packages/ui.web/pages/auth/signin.tsx
@@ -0,0 +1,98 @@
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { push } from 'connected-next-router';
+import Link from 'next/link';
+import { createContext, useContext, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import StyledLink from '../../components/StyledLink';
+import {
+  DEFAULT_PAGE_SEARCH_RESULTS,
+  DEFAULT_PAGE_START,
+} from '../../constants';
+
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
+
+export function Signin() {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const dispatch = useDispatch();
+
+  /** When changing query text */
+  const handleChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        height: '100vh',
+        margin: 0,
+        padding: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography fontWeight={500} variant="h1" mb={3}>
+          punku
+        </Typography>
+        <TextField
+          autoComplete='username'
+          fullWidth
+          placeholder='username'
+          id="username"
+          sx={{
+            ".MuiInputBase-root": {
+              borderBottomLeftRadius: "0px",
+              borderBottomRightRadius: "0px"
+            }
+          }}
+          value={username}
+          onChange={handleChangeUsername}
+        />
+        <TextField
+          placeholder='password'
+          autoComplete='off'
+          fullWidth
+          type="password"
+          id="password"
+          sx={{
+            ".MuiInputBase-root": {
+              borderTopLeftRadius: "0px",
+              borderTopRightRadius: "0px"
+            }
+          }}
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <Button fullWidth variant="contained" sx={{
+          my: 2
+        }}>
+          Sign in
+        </Button>
+        <Typography variant="body2">
+          <Link href="/auth/signup" passHref><StyledLink>Create an account</StyledLink></Link>
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
+
+export default Signin;
